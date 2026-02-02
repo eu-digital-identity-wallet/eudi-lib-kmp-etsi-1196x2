85b2684252dc21eaa6e03c8eced3b5c3301ce856
babisRoutis <haralampos.routis@netcompany.com>
2026-02-02 11:16:59 +0200

    Replace `AdvancedDataLoader` with `DataLoader` in `ObservableHttpLoader` and update related test logic.

--- consultation-dss/src/jvmAndAndroidTest/kotlin/eu/europa/ec/eudi/etsi1196x2/consultation/dss/ObservableHttpDownloader.kt
+++ consultation-dss/src/jvmAndAndroidTest/kotlin/eu/europa/ec/eudi/etsi1196x2/consultation/dss/ObservableHttpDownloader.kt
@@ -1,9 +1,24 @@
+/*
+ * Copyright (c) 2026 European Commission
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package eu.europa.ec.eudi.etsi1196x2.consultation.dss
 
-import eu.europa.esig.dss.spi.client.http.AdvancedDataLoader
+import eu.europa.esig.dss.spi.client.http.DataLoader
 import java.util.concurrent.atomic.AtomicInteger
 
-class ObservableHttpLoader(val proxied: AdvancedDataLoader) : AdvancedDataLoader by proxied {
+class ObservableHttpLoader(val proxied: DataLoader) : DataLoader by proxied {
     private val _callCount = AtomicInteger(0)
     val callCount: Int get() = _callCount.get()
 
@@ -14,5 +29,4 @@ class ObservableHttpLoader(val proxied: AdvancedDataLoader) : AdvancedDataLoader
         _callCount.incrementAndGet()
         return proxied.get(url)
     }
-
 }
