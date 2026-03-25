3cc73b25097095bd70601bafd3882718b9e93b03
babisRoutis <haralampos.routis@netcompany.com>
2026-03-25 20:55:17 +0200

    Add test utilities for EUDI reference implementation verification context
    Signed-off-by: babisRoutis <haralampos.routis@netcompany.com>

---
+++ 119602-consultation/src/jvmAndAndroidTest/kotlin/eu/europa/ec/eudi/etsi119602/consultation/EUDIRefImplEnv.kt
@@ -0,0 +1,116 @@
+/*
+ * Copyright (c) 2026 European Commission
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package eu.europa.ec.eudi.etsi119602.consultation
+
+import eu.europa.ec.eudi.etsi119602.ListOfTrustedEntities
+import eu.europa.ec.eudi.etsi119602.ListOfTrustedEntitiesClaims
+import eu.europa.ec.eudi.etsi1196x2.consultation.SensitiveApi
+import eu.europa.ec.eudi.etsi1196x2.consultation.SupportedLists
+import eu.europa.ec.eudi.etsi1196x2.consultation.VerificationContext
+import kotlinx.coroutines.test.runTest
+import kotlinx.io.files.Path
+import kotlinx.serialization.json.JsonObject
+import kotlin.test.Test
+import kotlin.test.assertContentEquals
+import kotlin.test.fail
+import kotlin.time.Duration.Companion.hours
+
+object EUDIRefImplEnv {
+
+    // https://trustedlist.serviceproviders.eudiw.dev/LOTE/json/RegistrarsAndRegisters.json
+    // https://trustedlist.serviceproviders.eudiw.dev/LOTE/json/PubEAAProviders.json
+
+    val LOTE_URL = SupportedLists(
+        pidProviders = "https://trustedlist.serviceproviders.eudiw.dev/LOTE/json/PIDProviders.json",
+        walletProviders = "https://trustedlist.serviceproviders.eudiw.dev/LOTE/json/WalletProviders.json",
+        wrpacProviders = "https://trustedlist.serviceproviders.eudiw.dev/LOTE/json/WRPACProviders.json",
+        wrprcProviders = "https://trustedlist.serviceproviders.eudiw.dev/LOTE/json/WRPRCProviders.json",
+    )
+}
+
+class EUDIRefImplEnvTest {
+    // this is a hack
+    val hackedParseJwt = run {
+        val jwsJson = ParseJwt.jwsJson<JsonObject, ListOfTrustedEntities>()
+        ParseJwt { json ->
+            val outcome = jwsJson(json)
+            when (outcome) {
+                is ParseJwt.Outcome.Parsed -> {
+                    val (h, p) = outcome
+                    ParseJwt.Outcome.Parsed(h, ListOfTrustedEntitiesClaims(p))
+                }
+
+                is ParseJwt.Outcome.ParseFailed -> outcome
+            }
+        }
+    }
+
+    @Test
+    @SensitiveApi
+    fun testDownload() = runTest {
+        createHttpClient().use { httpClient ->
+            val fileStore = LoTEFileStore(
+                cacheDirectory = Path(System.getProperty("java.io.tmpdir")!!, "ref-impl-lote"),
+            )
+            val loadLoTE = LoadSingleLoTEWithFileCache(
+                fileStore = fileStore,
+                downloadSingleLoTE = DownloadSingleLoTE(httpClient),
+                fileCacheExpiration = 24.hours,
+                parseJwt = hackedParseJwt,
+            )
+            // Get the LoTEs, organized them as EUDIW verification contexts
+            val provisionTrustAnchors = getTrustAnchorsProvisioner(loadLoTE, parseJwt = hackedParseJwt)
+            val isChainTrustedForContext = provisionTrustAnchors.nonCached(EUDIRefImplEnv.LOTE_URL)
+
+            val expectedContexts: List<VerificationContext> =
+                listOf(
+                    VerificationContext.PID,
+                    VerificationContext.PIDStatus,
+                    VerificationContext.WalletInstanceAttestation,
+                    VerificationContext.WalletUnitAttestation,
+                    VerificationContext.WalletUnitAttestationStatus,
+                    VerificationContext.WalletRelyingPartyAccessCertificate,
+                    VerificationContext.WalletRelyingPartyRegistrationCertificate,
+                    VerificationContext.WalletRelyingPartyRegistrationCertificateStatus,
+                )
+
+            val actualContexts = isChainTrustedForContext.supportedContexts
+            assertContentEquals(expectedContexts.sortedBy { it.toString() }, actualContexts.sortedBy { it.toString() })
+            val errors = mutableMapOf<VerificationContext, Throwable>()
+            actualContexts.forEach { ctx ->
+                try {
+                    when (val outcome = isChainTrustedForContext.getTrustAnchors(ctx)) {
+                        null -> println("$ctx : Not found")
+                        else -> println("$ctx : ${outcome.list.size} ")
+                    }
+                } catch (e: Exception) {
+                    errors[ctx] = e
+                }
+            }
+            if (errors.isNotEmpty()) {
+                val es = buildString {
+                    appendLine("Errors:")
+                    errors.forEach { (ctx, e) ->
+                        appendLine("$ctx ")
+                        e.suppressed.forEach { appendLine(" - $it") }
+                    }
+                }
+                fail(es)
+            }
+            fileStore.clear()
+        }
+    }
+}
