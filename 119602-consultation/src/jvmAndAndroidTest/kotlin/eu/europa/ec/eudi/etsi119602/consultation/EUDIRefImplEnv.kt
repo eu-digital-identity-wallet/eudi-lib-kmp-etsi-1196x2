ca78e8baa9eb5dd9115bb2c0766c432efbdb1575
babisRoutis <haralampos.routis@netcompany.com>
2026-04-08 14:04:27 +0300

    Replace external `Uri` import with internal `Uri` implementation in `EUDIRefImplEnv`
    Signed-off-by: babisRoutis <haralampos.routis@netcompany.com>

--- 119602-consultation/src/jvmAndAndroidTest/kotlin/eu/europa/ec/eudi/etsi119602/consultation/EUDIRefImplEnv.kt
+++ 119602-consultation/src/jvmAndAndroidTest/kotlin/eu/europa/ec/eudi/etsi119602/consultation/EUDIRefImplEnv.kt
@@ -15,9 +15,9 @@
  */
 package eu.europa.ec.eudi.etsi119602.consultation
 
-import com.eygraber.uri.Uri
 import eu.europa.ec.eudi.etsi119602.ListOfTrustedEntities
 import eu.europa.ec.eudi.etsi119602.ListOfTrustedEntitiesClaims
+import eu.europa.ec.eudi.etsi119602.Uri
 import eu.europa.ec.eudi.etsi1196x2.consultation.SensitiveApi
 import eu.europa.ec.eudi.etsi1196x2.consultation.SupportedLists
 import eu.europa.ec.eudi.etsi1196x2.consultation.VerificationContext
