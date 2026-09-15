8f91e658ebaa5d98cf377da8ac48340487143936
babisRoutis <haralampos.routis@netcompany.com>
2026-09-15 12:26:37 +0300

    Disable unstable network tests

--- 119602-consultation/src/jvmAndAndroidTest/kotlin/eu/europa/ec/eudi/etsi119602/consultation/EUDIRefImplEnv.kt
+++ 119602-consultation/src/jvmAndAndroidTest/kotlin/eu/europa/ec/eudi/etsi119602/consultation/EUDIRefImplEnv.kt
@@ -29,6 +29,7 @@ import kotlinx.io.files.Path
 import java.nio.file.Files
 import java.security.cert.TrustAnchor
 import java.security.cert.X509Certificate
+import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertContentEquals
 import kotlin.test.fail
@@ -50,6 +51,7 @@ object EUDIRefImplEnv {
 
 class EUDIRefImplEnvTest {
     @Test
+    @Ignore("Unstable network-based test")
     @SensitiveApi
     fun testDownload() = runTest {
         createHttpClient().use { httpClient ->
