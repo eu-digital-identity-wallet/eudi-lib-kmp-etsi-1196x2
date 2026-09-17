7696159bfea84f03dc8a7ee0d063e24c4ab3f993
Vafeiadis Nikos <nvafeiadis@netcompany.com>
2026-09-17 15:35:46 +0300

    Add JSON schemas for SIE and TIE, and replace `ServiceInformationExtensions` with `JsonArray` in TrustedEntity model

--- 119602-data-model/src/commonMain/kotlin/eu/europa/ec/eudi/etsi119602/datamodel/TrustedEntity.kt
+++ 119602-data-model/src/commonMain/kotlin/eu/europa/ec/eudi/etsi119602/datamodel/TrustedEntity.kt
@@ -102,7 +102,7 @@ public constructor(
     @SerialName(ETSI19602.SCHEME_SERVICE_DEFINITION_URI) val schemeServiceDefinitionURI: List<MultiLanguageURI>? = null,
     @SerialName(ETSI19602.SERVICE_SUPPLY_POINTS) val supplyPoints: List<ServiceSupplyPointURI>? = null,
     @SerialName(ETSI19602.SERVICE_DEFINITION_URI) val definitionURI: List<MultiLanguageURI>? = null,
-    @SerialName(ETSI19602.SERVICE_INFORMATION_EXTENSIONS) val informationExtensions: ServiceInformationExtensions? = null,
+    @SerialName(ETSI19602.SERVICE_INFORMATION_EXTENSIONS) val informationExtensions: JsonArray? = null,
 ) {
     init {
         with(Assertions) {
@@ -124,7 +124,7 @@ public constructor(
     @SerialName(ETSI19602.SERVICE_STATUS) @Required val status: Uri,
     @SerialName(ETSI19602.STATUS_STARTING_TIME) @Required val statusStartingTime: LoTEDateTime,
     @SerialName(ETSI19602.SERVICE_TYPE_IDENTIFIER) val typeIdentifier: Uri? = null,
-    @SerialName(ETSI19602.SERVICE_INFORMATION_EXTENSIONS) val informationExtensions: ServiceInformationExtensions? = null,
+    @SerialName(ETSI19602.SERVICE_INFORMATION_EXTENSIONS) val informationExtensions: JsonArray? = null,
 ) {
     init {
         with(Assertions) {
@@ -133,9 +133,6 @@ public constructor(
         }
     }
 }
-
-// TODO Provide an explicit data model for ServiceInformationExtensions
-public typealias ServiceInformationExtensions = JsonArray
 
 @Serializable
 public data class ServiceSupplyPointURI
